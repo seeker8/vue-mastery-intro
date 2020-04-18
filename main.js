8fe730e839d33fd953afe87917a369f9c1818a69
seeker8 <chuyo_gk87@yahoo.com.mx>
2020-04-18 13:55:56 -0400

    add variants list to data

--- main.js
+++ main.js
@@ -5,6 +5,16 @@ var app = new Vue({
         image: './assets/vmSocks-green-onWhite.jpg',
         inStock: true,
         details: ["80% cotton", "20% polyester", "Gender-neutral"],
+        variants: [
+            {
+                variantId: 2234,
+                variantColor: "green"
+            },
+            {
+                variantId: 2235,
+                variantColor: "blue"
+            }
+        ],
         description: 'A pair of warm, fuzzy socks',
         link: 'https://www.vuemastery.com/courses/intro-to-vue-js/attribute-binding',
         onSale: true
