252fc665db05f86f427ce4b27601bbbb17e451b7
seeker8 <chuyo_gk87@yahoo.com.mx>
2020-04-18 13:45:39 -0400

    challenge: add on sale property to use as conditional

--- main.js
+++ main.js
@@ -5,6 +5,7 @@ var app = new Vue({
         description: 'A pair of warm, fuzzy socks',
         image: './assets/vmSocks-green-onWhite.jpg',
         inStock: true,
+        onSale: true,
         link: 'https://www.vuemastery.com/courses/intro-to-vue-js/attribute-binding'
     }
 })
