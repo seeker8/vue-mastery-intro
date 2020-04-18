39dd13b0739827d782b91d01304f256734c151e5
seeker8 <chuyo_gk87@yahoo.com.mx>
2020-04-18 14:23:45 -0400

    add variantImage to variants

--- main.js
+++ main.js
@@ -8,11 +8,13 @@ var app = new Vue({
         variants: [
             {
                 variantId: 2234,
-                variantColor: "green"
+                variantColor: "green",
+                variantImage: "./assets/vmSocks-green-onWhite.jpg"
             },
             {
                 variantId: 2235,
-                variantColor: "blue"
+                variantColor: "blue",
+                variantImage: "./assets/vmSocks-blue-onWhite.jpg"
             }
         ],
         cart: 0,
