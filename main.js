f296518bd3b963336d00e81930a50ac943298260
seeker8 <chuyo_gk87@yahoo.com.mx>
2020-04-18 19:08:20 -0400

    add review component and trigger submit event to product

--- main.js
+++ main.js
@@ -1,3 +1,4 @@
+Vue.config.devtools = true;
 Vue.component('product-details', {
     props: {
         details: {
@@ -42,6 +43,7 @@ Vue.component('product', {
             Cart</button>
         <button @click="removeFromCart">Remove</button>
     </div>
+    <product-review></product
 </div>
     `,
     data: function () {
@@ -93,6 +95,51 @@ Vue.component('product', {
         },
         shipping: function () {
             return this.premium ? 'free' : 2.99;
+        }
+    }
+});
+
+Vue.component('product-review', {
+    template: `
+    <form class="review-form" @submit.prevent="onSubmit>
+       <p>
+          <label for="name">Name:</label>
+          <input id="name" v-model="name">
+       </p>
+       <p>
+          <label for="review">Review:</label>
+          <textarea id="review" v-model="review"></textarea>
+       </p>
+       <p>
+          <label for="rating">Rating:</label>
+          <select id="rating" v-model.number="rating">
+             <option>5</option>
+             <option>4</option>
+             <option>3</option>
+             <option>2</option>
+             <option>1</option>
+          </select>
+       </p>
+    </form>
+    `,
+    data: function () {
+        return {
+            name: null,
+            review: null,
+            rating: null
+        }
+    },
+    methods: {
+        onSubmit: function () {
+            let productReview = {
+                name: this.name,
+                review: this.review,
+                rating: this.rating
+            };
+            this.$emit('review-submitted', productReview);
+            this.name = null;
+            this.review = null;
+            this.rating = null;
         }
     }
 });
