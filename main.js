3d81b9f353026158689f886b8bc5a6f1c12b723c
seeker8 <chuyo_gk87@yahoo.com.mx>
2020-04-18 20:29:58 -0400

    add tab component

--- main.js
+++ main.js
@@ -197,6 +197,21 @@ Vue.component('product-review', {
     }
 });
 
+Vue.component('product-tabs', {
+    template: `
+      <div>
+        <span class="tab" v-for="(tab, index) in tabs" :key="index">
+           {{tab}}
+        </span>
+      </div>
+    `,
+    data: function () {
+        return {
+            tabs: ['Reviews', 'Make a review']
+        }
+    }
+})
+
 var app = new Vue({
     el: '#app',
     data: {
